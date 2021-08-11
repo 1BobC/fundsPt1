f41ef2388e64921b1cf5c0d7f2912245c100e4f3
1BObC <bob.cocksedge@gmail.com>
2021-08-11 22:04:29 +0100

    Create project files and do 'a variable' exercises

---
+++ jscript.js
@@ -0,0 +1,19 @@
+// just the basics to start!
+console.log('Hello from roco using touch typing!');
+
+//using let
+let message;
+message = 'Hello again';
+alert(message);
+//or like this
+let message2 = 'Hello to you';
+alert(message2);
+console.log(message2);
+
+//multi declaration
+
+let name='/Harry/ ';
+let age=25;
+let message3=' /Hello from Harry/ ';
+alert(name + age + message3);
+console.log(message3, name, age);
